9686b2959db6e9cc27cc33fb150bb9ba9a272dfc
alvarof2 <fernandez.f.alvaro@gmail.com>
2024-01-18 21:38:55 +0100

    Tests and README

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ build:
 	GOOS=darwin GOARCH=amd64 go build -o bin/image-annotator-webhook-darwin-amd64 .
 	GOOS=linux GOARCH=amd64 go build -o bin/image-annotator-webhook-linux-amd64 .
 
+.PHONY: test
+test:
+	@echo "\n🛠️  Running unit tests..."
+	go test ./...
---
+++ main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+// Source https://stackoverflow.com/a/33404435
+// Exit with return code 1 if env. var. is not provided
+func TestApiTokenCrash(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		main()
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestApiTokenCrash")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+var collector = newSocketCollector()
+var ch = make(chan prometheus.Metric)
+
+// Count that the number of metrics is greater than 1
+func TestCollectAndCount(t *testing.T) {
+
+	updateMetrics()
+
+	number := testutil.CollectAndCount(collector, "socket_score")
+	if number < 1 {
+		t.Fatalf("Less than 1 metric was returned. Only %d metrics returned", number)
+	}
+
+}
+
+// Check linter on metrics
+func TestCollectndLint(t *testing.T) {
+
+	problem, err := testutil.CollectAndLint(collector, "socket_score")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+		t.Errorf("%s", problem)
+	}
+
+}
